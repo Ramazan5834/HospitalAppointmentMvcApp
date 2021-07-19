19268af711897a39e3e18e04c03f966dcfe5e84f
Ramazan58 <iscananramazan@gmail.com>
2021-07-19 17:45:35 +0300

    Add project files.

---
+++ HospitalAppointment.UI/Areas/Doctor/Controllers/BlackListController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using HospitalAppointment.Business.Interfaces;
+using HospitalAppointment.DataAccess.Concrete.EntityFrameworkCore.Entities;
+using HospitalAppointment.DTO.DTOs.BlackList;
+using HospitalAppointment.UI.StringInfo;
+using HospitalAppointment.UI.Tools.ActiveUserContext;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+
+namespace HospitalAppointment.UI.Areas.Doctor.Controllers
+{
+    [Authorize(Roles = RoleInfo.Doctor)]
+    [Area(AreaInfo.Doctor)]
+    public class BlackListController : Controller
+    {
+        private ActiveDoctor _activeDoctor;
+        private readonly IMapper _mapper;
+        private readonly IBlackListService _blackListService;
+        private readonly IUserService _userService;
+        private readonly IAppointmentService _appointmentService;
+        public BlackListController(IAppointmentService appointmentService,IBlackListService blackListService, IMapper mapper, IHttpContextAccessor httpContextAccessor, IUserService userService, IDoctorService doctorService)
+        {
+            _activeDoctor = new ActiveDoctor(httpContextAccessor, userService, doctorService);
+            _mapper = mapper;
+            _blackListService = blackListService;
+            _userService = userService;
+            _appointmentService = appointmentService;
+        }
+        public bool CreateBlackList(int patientId, int doctorId, DateTime appDateTime)
+        {
+            var nowAppointment = _appointmentService.GetAppointmentByPatientIdAndDateTime(patientId, appDateTime);
+            nowAppointment.Confirmed = true;
+            _appointmentService.Update(nowAppointment);
+            var addBlackList = new BlackList()
+            {
+                PatientId = patientId,
+                DoctorId = doctorId,
+                DeceptionCount = DateTime.Now.AddDays(7),
+                Description = _userService.GetById(doctorId).Name + " " + _userService.GetById(doctorId).SurName + "tarafından karaliste oluşturuldu"
+            };
+            try
+            {
+                _blackListService.Add(addBlackList);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ HospitalAppointment.UI/Areas/Doctor/Controllers/PrescribeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using HospitalAppointment.Business.Interfaces;
+using HospitalAppointment.DataAccess.Concrete.EntityFrameworkCore.Entities;
+using HospitalAppointment.DTO.DTOs.Prescribe;
+using HospitalAppointment.UI.StringInfo;
+using HospitalAppointment.UI.Tools.ActiveUserContext;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+
+namespace HospitalAppointment.UI.Areas.Doctor.Controllers
+{
+    [Authorize(Roles = RoleInfo.Doctor)]
+    [Area(AreaInfo.Doctor)]
+    public class PrescribeController : Controller
+    {
+        private ActiveDoctor _activeDoctor;
+        private readonly IMapper _mapper;
+        private readonly IPrescribeService _prescribeService;
+        private readonly IAppointmentService _appointmentService;
+        public PrescribeController(IAppointmentService appointmentService, IPrescribeService prescribeService, IMapper mapper, IHttpContextAccessor httpContextAccessor, IUserService userService, IDoctorService doctorService)
+        {
+            _activeDoctor = new ActiveDoctor(httpContextAccessor, userService, doctorService);
+            _mapper = mapper;
+            _prescribeService = prescribeService;
+            _appointmentService = appointmentService;
+        }
+        public IActionResult CreatePrescribe(int patientId, int doctorId, string appDateTime)
+        {
+            DateTime appDateTimeLocal = Convert.ToDateTime(appDateTime);
+            var nowAppointment = _appointmentService.GetAppointmentByPatientIdAndDateTime(patientId, appDateTimeLocal);
+            nowAppointment.Confirmed = true;
+            _appointmentService.Update(nowAppointment);
+            TempData["ActiveDoctor"] = _activeDoctor.GetActiveDoctor();
+
+            var addPrescribeDto = new PrescribeAddDto()
+            {
+                PatientId = patientId,
+                DoctorId = doctorId
+            };
+            return View(addPrescribeDto);
+        }
+
+        [HttpPost]
+        public IActionResult CreatePrescribe(PrescribeAddDto prescribeAddDto)
+        {
+            var prescribe = _mapper.Map<Prescribe>(prescribeAddDto);
+            _prescribeService.Add(prescribe);
+            return RedirectToAction("TodayAppointments", "Appointment");
+        }
+    }
+}
